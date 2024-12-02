378995cfc759fd222bfa48c3867389f06fd6f9a9
SereMark <seremark056@gmail.com>
2024-12-02 11:36:31 +0100

    refactor: improve code readability and structure in ResidualUnit and ChessModel classes

--- src/models/model.py
+++ src/models/model.py
@@ -3,49 +3,54 @@ import torch, torch.nn as nn
 class ResidualUnit(nn.Module):
     def __init__(self, in_channels: int, out_channels: int, stride: int = 1) -> None:
         super().__init__()
-        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.conv1 = nn.Conv2d(
+            in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False
+        )
         self.norm1 = nn.BatchNorm2d(out_channels)
         self.relu = nn.ReLU(inplace=True)
-        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1, bias=False)
+        self.conv2 = nn.Conv2d(
+            out_channels, out_channels, kernel_size=3, padding=1, bias=False
+        )
         self.norm2 = nn.BatchNorm2d(out_channels)
-        self.downsample = None
-        if stride != 1 or in_channels != out_channels:
-            self.downsample = nn.Sequential(
-                nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride, bias=False),
+        self.downsample = (
+            nn.Sequential(
+                nn.Conv2d(
+                    in_channels, out_channels, kernel_size=1, stride=stride, bias=False
+                ),
                 nn.BatchNorm2d(out_channels),
             )
+            if stride != 1 or in_channels != out_channels
+            else None
+        )
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         identity = x
 
-        out = self.conv1(x)
-        out = self.norm1(out)
-        out = self.relu(out)
-
-        out = self.conv2(out)
-        out = self.norm2(out)
+        out = self.relu(self.norm1(self.conv1(x)))
+        out = self.norm2(self.conv2(out))
 
         if self.downsample is not None:
             identity = self.downsample(x)
 
         out += identity
-        out = self.relu(out)
+        return self.relu(out)
 
-        return out
 
 class ChessModel(nn.Module):
     def __init__(self, num_moves: int, filters: int = 64, res_blocks: int = 5) -> None:
         super().__init__()
         self.num_moves = num_moves
+
         self.initial_block = nn.Sequential(
             nn.Conv2d(20, filters, kernel_size=3, padding=1, bias=False),
             nn.BatchNorm2d(filters),
             nn.ReLU(inplace=True),
         )
+
         self.residual_layers = nn.Sequential(
             *[ResidualUnit(filters, filters) for _ in range(res_blocks)]
         )
-        
+
         self.policy_head = nn.Sequential(
             nn.Conv2d(filters, 2, kernel_size=1, bias=False),
             nn.BatchNorm2d(2),
@@ -53,7 +58,7 @@ class ChessModel(nn.Module):
             nn.Flatten(),
             nn.Linear(2 * 8 * 8, self.num_moves),
         )
-        
+
         self.value_head = nn.Sequential(
             nn.Conv2d(filters, 1, kernel_size=1, bias=False),
             nn.BatchNorm2d(1),
@@ -64,7 +69,10 @@ class ChessModel(nn.Module):
             nn.Linear(64, 1),
             nn.Tanh(),
         )
-        
+
+        self._initialize_weights()
+
+    def _initialize_weights(self) -> None:
         for module in self.modules():
             if isinstance(module, (nn.Conv2d, nn.Linear)):
                 nn.init.kaiming_normal_(module.weight, nonlinearity="relu")
