ce34bccf1bf27b981a9b445cdfd6b66a74b83ee3
SereMark <seremark056@gmail.com>
2025-01-30 18:43:32 +0100

    refactor: update LichessBotDeploymentWorker constructor to include additional parameters for configuration

--- src/lichess_deployment/lichess_bot_deployment_worker.py
+++ src/lichess_deployment/lichess_bot_deployment_worker.py
@@ -1,2 +1,3 @@
 class LichessBotDeploymentWorker:
-    def __init__(self): raise NotImplementedError("Not implemented")
+    def __init__(self, model_path, opening_book_path, lichess_token, time_control, rating_range, use_mcts, cloud_provider, progress_callback, status_callback):
+        raise NotImplementedError
